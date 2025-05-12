d77721243090d540861b7aa6ab3a8a40badc6f27
Eliana Depietri <eliana.depietri@gmail.com>
2025-05-12 18:33:22 +0200

    -day 1 completed

--- server/app.js
+++ server/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cors = require("cors")
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
+const  students = require("./students.json");
+const  cohorts = require("./cohorts.json");
 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
@@ -15,6 +17,7 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static("public"));
@@ -29,8 +32,15 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get("/api/students", (req, res) => {
+  res.json(students);
+})
+
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+})
 
 // START SERVER
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port http://localhost:${PORT}`);
 });
